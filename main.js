2a57d0d7fa9ce39415c91e8c23ecc2463a2bc376
= <sfyire@gmail.com>
2015-01-12 21:22:29 +0000

    changed log format, added log watch her

--- main.js
+++ main.js
@@ -1,30 +1,44 @@
 (function(){
+	'use strict';
 	var fs = require('fs');
 	function HearthStoneLogger(){
 		var that = {},
 		configFile = 'log.config',
-
-		getConfigDir = function(){
+		getDataLocation = function(){
+			//TODO unhardcode
+			return 'C:/Program Files (x86)/Hearthstone/Hearthstone_Data/output_log.txt';
+		},
+		getConfigLocation = function(){
 			//TODO osx
-			return process.env.LOCALAPPDATA+'/Blizzard/Hearthstone';
+			return process.env.LOCALAPPDATA+'/Blizzard/Hearthstone/'+configFile;
 		},
 		configExist = function(){
-			return fs.existsSync(getConfigDir()+'/'+configFile);
+			return fs.existsSync(getConfigLocation());
 		},
 		createConfig = function(callback){
 			//TODO 32bit && osx 
 			if (configExist()) return; 
 			fs.renameSync(
 				process.cwd()+'/'+configFile,
-				getConfigDir()+'/'+configFile
+				getConfigLocation()
 				);
+		},
+		parseEvents = function(){
+			alert('called');
+		},
+		monitorChanges = function(){
+			fs.watchFile(getDataLocation(),parseEvents);
+			//here I need to detect when an arena
+			//pick event is happening 
+			//then store the pick till win/lose condition happens
 		}
+
 		;
+		that.monitorChanges = monitorChanges; 
 		that.createConfig = createConfig;
 		return that; 
 	}
-
-
-	HearthStoneLogger().createConfig(); 
-
+	var hearth = new HearthStoneLogger(); 
+	hearth.createConfig();
+	hearth.monitorChanges();
 })();
